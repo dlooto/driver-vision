aeae646e19667d32c7a61b51135b265e1392946c
Junn <junn@dondon.im>
2015-12-05 20:24:36 +0800

    添加多路牌参数设置界面: 暂缺js及相应处理函数. 待xiyun确认...

--- apps/vision/urls.py
+++ apps/vision/urls.py
@@ -7,10 +7,14 @@
 from django.conf.urls import patterns, url
 
 from vision import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     '',
     url(r"^$",          views.ParamsIndex.as_view(), name="vision:params_index"),
     url(r'^/set_params',views.set_params,            name="set_params"),
+    
+    url(r"^/single_board$", TemplateView.as_view(template_name='admin/single_board_params.html')),  #testing
+    url(r"^/multi_board$", TemplateView.as_view(template_name='admin/multi_board_params.html')),  #testing
     
 )
